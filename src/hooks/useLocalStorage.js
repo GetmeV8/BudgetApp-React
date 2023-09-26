e737729d92f2bf21749b651975dfeb6823f5a4ac
rameez <rameez.manoli786@gmail.com>
2023-09-26 14:26:38 +0530

    update

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react"
 
 export default function useLocalStorage(key, defaultValue) {
-  const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(key)
-    if (jsonValue != null) return JSON.parse(jsonValue)
+  // const [value, setValue] = useState(() => {
+  //   const jsonValue = localStorage.getItem(key)
+  //   if (jsonValue != null) return JSON.parse(jsonValue)
 
-    if (typeof defaultValue === "function") {
-      return defaultValue()
-    } else {
-      return defaultValue
-    }
-  })
+  //   if (typeof defaultValue === "function") {
+  //     return defaultValue()
+  //   } else {
+  //     return defaultValue
+  //   }
+  // })
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value))
